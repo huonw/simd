fe3a5d4fdb206dbbb546d9a08f2f5b9e38aa51e2
Huon Wilson <dbau.pp+github@gmail.com>
2015-08-12 14:26:16 -0700

    Fix feature name.

--- src/lib.rs
+++ src/lib.rs
@@ -501,9 +501,9 @@ pub mod sse2;
 pub mod sse3;
 #[cfg(target_feature = "ssse3")]
 pub mod ssse3;
-#[cfg(target_feature = "sse4_1")]
+#[cfg(target_feature = "sse4.1")]
 pub mod sse4_1;
-#[cfg(target_feature = "sse4_2")]
+#[cfg(target_feature = "sse4.2")]
 pub mod sse4_2;
 #[cfg(target_feature = "avx")]
 pub mod avx;
