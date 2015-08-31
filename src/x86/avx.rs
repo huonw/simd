d7a1be4d1a4e67f697bac3a6eee5524561b7464b
Huon Wilson <dbau.pp+github@gmail.com>
2015-08-31 16:31:17 -0700

    Expose most 128-bit AVX operations via traits.

--- src/x86/avx.rs
+++ src/x86/avx.rs
@@ -47,3 +47,73 @@ extern "platform-intrinsic" {
     fn x86_mm256_testz_pd(x: f64x4, y: f64x4) -> i32;
     fn x86_mm256_testz_si256(x: u64x4, y: u64x4) -> i32;
 }
+
+// 128-bit vectors:
+
+// 32 bit floats
+
+pub trait AvxF32x4 {
+    fn permutevar(self, other: i32x4) -> f32x4;
+}
+impl AvxF32x4 for f32x4 {
+    fn permutevar(self, other: i32x4) -> f32x4 {
+        unsafe { x86_mm_permutevar_ps(self, other) }
+    }
+}
+
+pub trait AvxBool32fx4 {}
+impl AvxBool32fx4 for bool32fx4 {}
+
+// 64 bit floats
+
+pub trait AvxF64x2 {
+    fn permutevar(self, other: i64x2) -> f64x2;
+}
+impl AvxF64x2 for f64x2 {
+    fn permutevar(self, other: i64x2) -> f64x2 {
+        unsafe { x86_mm_permutevar_pd(self, other) }
+    }
+}
+
+pub trait AvxBool64fx2 {}
+impl AvxBool64fx2 for bool64fx2 {}
+
+// 64 bit integers
+
+pub trait AvxU64x2 {}
+impl AvxU64x2 for u64x2 {}
+pub trait AvxI64x2 {}
+impl AvxI64x2 for i64x2 {}
+
+pub trait AvxBool64ix2 {}
+impl AvxBool64ix2 for bool64ix2 {}
+
+// 32 bit integers
+
+pub trait AvxU32x4 {}
+impl AvxU32x4 for u32x4 {}
+pub trait AvxI32x4 {}
+impl AvxI32x4 for i32x4 {}
+
+pub trait AvxBool32ix4 {}
+impl AvxBool32ix4 for bool32ix4 {}
+
+// 16 bit integers
+
+pub trait AvxU16x8 {}
+impl AvxU16x8 for u16x8 {}
+pub trait AvxI16x8 {}
+impl AvxI16x8 for i16x8 {}
+
+pub trait AvxBool16ix8 {}
+impl AvxBool16ix8 for bool16ix8 {}
+
+// 8 bit integers
+
+pub trait AvxU8x16 {}
+impl AvxU8x16 for u8x16 {}
+pub trait AvxI8x16 {}
+impl AvxI8x16 for i8x16 {}
+
+pub trait AvxBool8ix16 {}
+impl AvxBool8ix16 for bool8ix16 {}
