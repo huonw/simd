1eec5481525c74d944084a3ed39fa1ceadb2dda1
Huon Wilson <dbau.pp+github@gmail.com>
2015-08-31 14:37:49 -0700

    Autogenerate the intrinsic definitions to match rust-lang/rust#28079.

--- src/x86/sse3.rs
+++ src/x86/sse3.rs
@@ -3,10 +3,10 @@ use super::super::*;
 
 #[allow(dead_code)]
 extern "platform-intrinsic" {
-    fn x86_mm_addsub_pd(x: f64x2, y: f64x2) -> f64x2;
     fn x86_mm_addsub_ps(x: f32x4, y: f32x4) -> f32x4;
-    fn x86_mm_hadd_pd(x: f64x2, y: f64x2) -> f64x2;
+    fn x86_mm_addsub_pd(x: f64x2, y: f64x2) -> f64x2;
     fn x86_mm_hadd_ps(x: f32x4, y: f32x4) -> f32x4;
-    fn x86_mm_hsub_pd(x: f64x2, y: f64x2) -> f64x2;
+    fn x86_mm_hadd_pd(x: f64x2, y: f64x2) -> f64x2;
     fn x86_mm_hsub_ps(x: f32x4, y: f32x4) -> f32x4;
+    fn x86_mm_hsub_pd(x: f64x2, y: f64x2) -> f64x2;
 }
--- src/x86/ssse3.rs
+++ src/x86/ssse3.rs
@@ -3,9 +3,9 @@ use bitcast;
 
 #[allow(dead_code)]
 extern "platform-intrinsic" {
+    fn x86_mm_abs_epi8(x: i8x16) -> i8x16;
     fn x86_mm_abs_epi16(x: i16x8) -> i16x8;
     fn x86_mm_abs_epi32(x: i32x4) -> i32x4;
-    fn x86_mm_abs_epi8(x: i8x16) -> i8x16;
     fn x86_mm_hadd_epi16(x: i16x8, y: i16x8) -> i16x8;
     fn x86_mm_hadd_epi32(x: i32x4, y: i32x4) -> i32x4;
     fn x86_mm_hadds_epi16(x: i16x8, y: i16x8) -> i16x8;
@@ -14,10 +14,9 @@ extern "platform-intrinsic" {
     fn x86_mm_hsubs_epi16(x: i16x8, y: i16x8) -> i16x8;
     fn x86_mm_maddubs_epi16(x: i8x16, y: i8x16) -> i16x8;
     fn x86_mm_mulhrs_epi16(x: i16x8, y: i16x8) -> i16x8;
-    fn x86_mm_shuffle_epi8(x: u8x16, y: u8x16) -> u8x16;
-    fn x86_mm_sign_epi16(x: i16x8, y: i16x8) -> i16x8;
-    fn x86_mm_sign_epi32(x: i32x4, y: i32x4) -> i32x4;
+    fn x86_mm_shuffle_epi8(x: i8x16, y: i8x16) -> i8x16;
     fn x86_mm_sign_epi8(x: i8x16, y: i8x16) -> i8x16;
+    fn x86_mm_sign_epi16(x: i16x8, y: i16x8) -> i16x8;
 }
 
 pub trait Ssse3I8x16 {
@@ -30,7 +29,7 @@ pub trait Ssse3U8x16 {
 impl Ssse3U8x16 for u8x16 {
     #[inline]
     fn shuffle_bytes(self, indices: Self) -> Self {
-        unsafe {x86_mm_shuffle_epi8(self, indices)}
+        unsafe {bitcast(x86_mm_shuffle_epi8(bitcast(self), bitcast(indices)))}
     }
 }
 
