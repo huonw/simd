e5cc501f78ea546b3e579dd1c7fc2aafdd070969
Huon Wilson <dbau.pp+github@gmail.com>
2015-08-31 18:16:33 -0700

    Correct mistakes in x86 intrinsics up to SSE4.1.
    This requires https://github.com/rust-lang/rust/pull/28136.

--- src/x86/sse3.rs
+++ src/x86/sse3.rs
@@ -1,7 +1,6 @@
 use sixty_four::*;
 use super::super::*;
 
-#[allow(dead_code)]
 extern "platform-intrinsic" {
     fn x86_mm_addsub_ps(x: f32x4, y: f32x4) -> f32x4;
     fn x86_mm_addsub_pd(x: f64x2, y: f64x2) -> f64x2;
